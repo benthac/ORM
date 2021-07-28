cf03901f51399fb023dee940104bf18525918e33
ben thacker <benthac@gmail.com>
2021-07-28 16:33:14 -0400

    updating models

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,24 +4,48 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll()
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create({
+
+  })
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  User.update(req.id, {
+    where: {
+      id: req.params.id
+    }
+  })
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
   // delete a category by its `id` value
 });
 
